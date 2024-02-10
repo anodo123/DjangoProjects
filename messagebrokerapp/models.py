9860a30157a30bb22ba0a17319fa495ec2a8602f
AHMAD FAIZ <40581230+anodo123@users.noreply.github.com>
2024-02-10 23:34:18 +0530

    added new project which will help me understand message broking services in Django, here in case, it implements RabbitMQ messaging broker

---
+++ messagebrokerapp/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from uuid import uuid
+from django.contrib.auth.models import AbstractUser
+# Create your models here.
+class Post(models.Model):
+    post_id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length = 50)
+    content = models.TextField()
+
+class Comment(models.Model):
+    comment_id = models.AutoField(primary_key=True)
+    post_id = models.ForeignKey(Post.post_id, on_delete=models.CASCADE)
+    comment =  models.CharField(max_length =100)
+    author = models.CharField(max_length =50,)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class User(AbstractUser):
+    # Add your custom fields here
+    age = models.PositiveIntegerField(null=True, blank=True)
+    is_author = models.BinaryField(default = 0)
+    # Add more fields as needed
