096bdacb47fe2b2d560a2d1d3d5b0d9dd0e83796
AHMAD FAIZ <40581230+anodo123@users.noreply.github.com>
2024-02-12 22:56:40 +0530

    added blog_adding api

--- messagebrokerapp/views.py
+++ messagebrokerapp/views.py
@@ -1,19 +1,29 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from requests import request
 # Create your views here.
-
+from .models import Post, Comment, User
 
 
 @api_view(['POST','GET'])
 def add_post(request):
-    pass
-
-
-
+    try:
+        blog_id = None
+        blog_status = False
+        content = request.POST.get("post","")
+        title = request.POST.get("title","")
+        blog_post = Post(content = content,title = title)
+        if blog_post:
+            blog_post.save()
+            blog_id = blog_post.post_id
+        if blog_id:
+            blog_status = True
+        return JsonResponse({"blog_created":blog_status,"blog_id":blog_id})
+    except Exception as error:
+        return JsonResponse({"blog_created":False,"blog_id":None})
 
 
 @api_view(['POST','GET'])
